a35a3cf3953ca790f456ff82a9b0a2187340f72b
ziqingma <74625412+ziqingma@users.noreply.github.com>
2021-03-29 18:37:34 +0200

    Update orange_avoider_group9.h

--- sw/airborne/modules/orange_avoider_group9/orange_avoider_group9.h
+++ sw/airborne/modules/orange_avoider_group9/orange_avoider_group9.h
@@ -21,16 +21,16 @@ extern float green_color_count_frac;        // Green colour fraction threshold
 extern float orange_color_count_frac;       // Orange colour fraction threshold
 
 //extern int16_t obstacle_free_confidence;
-//extern int16_t left_free_confidence;
-//extern int16_t right_free_confidence;
-extern float central_coefficient;           // Weight determining how much importance should be give to the central part of the image
 extern int16_t n_trajectory_confidence;     // Forward trajectory confidence threshold
-extern int16_t n_turning_confidence;        // Turning trajectory confidence threshold
 
-extern uint32_t left_green_count;           // Green pixels in bottom left
-extern uint32_t right_green_count;          // Green pixels in bottom right
+extern uint32_t orange_color_count;          // Orange pixels
+extern uint32_t green_color_count;          // green pixels
 extern uint32_t left_orange_count;          // Orange pixels in bottom left
 extern uint32_t right_orange_count;         // Orange pixels in bottom right
+extern uint32_t left_green_count;           // Green pixels in bottom left
+extern uint32_t right_green_count;          // Green pixels in bottom right
+extern float central_coefficient;           // Weight determining how much importance should be give to the central part of the image
+extern int16_t n_turning_confidence;        // Turning trajectory confidence threshold
 
 // functions
 extern void orange_avoider_init(void);
